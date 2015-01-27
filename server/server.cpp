a69cc5ea8e694b3a74d8e2fd79b4f1e8a64bc91f
Atyansh <atyansh.jaiswal@gmail.com>
2015-01-27 11:43:31 -0800

    Started basic Server header file. Commiting because I'm changing workspace.

--- server/server.cpp
+++ server/server.cpp
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "server.h"
 
 int main(int argc, char * argv[]) {
--- server/server.h
+++ server/server.h
@@ -1,1 +1,23 @@
+#ifndef SERVER
+#define SERVER
 
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+class Server {
+  private:
+    struct sockaddr_in address;
+
+    int sock;
+
+  public:
+    Server();
+    ~Server();
+    
+}
+
+
+#endif
